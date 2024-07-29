35a9c3e83ceba979fff937c95d67b89f745fbe26
Jiri Bednar <jbednar@isocta.com>
2024-07-29 20:31:36 +1000

    Service troubleshooting

--- service.py
+++ service.py
@@ -1,6 +1,7 @@
 # service.py
 from jnius import autoclass, PythonJavaClass, java_method
 import time
+import threading
 
 PythonService = autoclass('org.kivy.android.PythonService')
 Context = autoclass('android.content.Context')
@@ -36,8 +37,15 @@ def start_service():
     receiver = SMSReceiver()
     intent_filter = IntentFilter('android.provider.Telephony.SMS_RECEIVED')
     context.registerReceiver(receiver, intent_filter)
-    while True:
-        time.sleep(1)
+
+    # Running an infinite loop in a separate thread
+    def run_service():
+        while True:
+            time.sleep(1)
+
+    service_thread = threading.Thread(target=run_service)
+    service_thread.daemon = True  # Daemonize thread to ensure it exits when the main program exits
+    service_thread.start()
 
 if __name__ == '__main__':
     start_service()
