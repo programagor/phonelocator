a59b7221f3dfd70e3ad848fcc80f08c06b63f0a5
Jiri Bednar <jbednar@isocta.com>
2024-07-29 19:04:13 +1000

    Service debug

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from kivy.app import App
 from kivy.uix.label import Label
 from jnius import autoclass, cast
 
-PythonService = autoclass('org.kivy.android.PythonService')
 PythonActivity = autoclass('org.kivy.android.PythonActivity')
 Intent = autoclass('android.content.Intent')
+Service = autoclass('org.kivy.android.PythonService')
 
 class SMSListenerApp(App):
     def build(self):
@@ -13,7 +13,6 @@ class SMSListenerApp(App):
         return Label(text="SMS Listener App Running")
 
     def start_service(self):
-        # Start the background service if it's not already running
         service_class = autoclass('org.kivy.android.PythonService')
         service_intent = Intent(PythonActivity.mActivity, service_class)
         PythonActivity.mActivity.startService(service_intent)
