68335d18719794d42996c6fe04ef5a76eb32bd10
Jiri Bednar <jbednar@isocta.com>
2024-07-30 09:46:25 +1000

    Service troubleshoot

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ class MainApp(App):
     def start_service(self):
         PythonActivity = autoclass('org.kivy.android.PythonActivity')
         Intent = autoclass('android.content.Intent')
+        PythonService = autoclass('org.kivy.android.PythonService')
         
         mActivity = PythonActivity.mActivity
         context = cast('android.content.Context', mActivity.getApplicationContext())
         
-        serviceIntent = Intent(context, autoclass('org.kivy.android.PythonService').class_)
+        serviceIntent = Intent(context, PythonService)
         context.startService(serviceIntent)
 
 if __name__ == '__main__':
